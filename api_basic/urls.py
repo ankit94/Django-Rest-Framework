5faa387e07fcda342ae72837870d27def8f093ce
ankit94 <ankit220694@gmail.com>
2020-06-18 10:54:08 -0700

    initial commit

---
+++ api_basic/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('article', views.ArticleModelViewset, basename='article')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('<int:pk>', include(router.urls)),
+    # path('article/', views.article_list, name='article'),
+    # path('article/', views.ArticleAPIView.as_view(), name='article'),
+    # path('article/detail/<int:id>', views.ArticleDetailAPIView.as_view(), name='article_detail'),
+    # path('generic/article/<int:id>', views.GenericAPIView.as_view(), name='generic_article'),
+]
